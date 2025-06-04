d85ba3b524c2ec8f747014c6fc5a8d6014e33118
mlpierce22 <mlpierce22@gmail.com>
2025-06-04 13:34:38 -0700

    Update config with default values and update the default added metrics

---
+++ cli/trainloop_cli/scaffold/trainloop/eval/suites/is_helpful.py
@@ -0,0 +1,5 @@
+from ..metrics import is_helpful
+from ..helpers import tag
+
+# You can define as many metrics as you like to test against and chain them here. These will run on every sample matching "my-tag".
+results = tag("my-tag").check(is_helpful)
