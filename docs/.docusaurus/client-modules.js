54fd2ec9fa9593320f613c91e121027ad54e384c
mlpierce22 <mlpierce22@users.noreply.github.com>
2025-07-10 18:17:46 +0000

    Deploy website - based on 2eea5cb4793634fb03e14fcb516f525cda3291fe

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/linuxbrew/workspace/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/linuxbrew/workspace/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/linuxbrew/workspace/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/linuxbrew/workspace/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/linuxbrew/workspace/docs/src/css/custom.css"),
-];
